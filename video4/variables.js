42b603743c5ed35bee160e27186ea03670e5109b
Ayush Vishwakarma <161769397+Ayushvik@users.noreply.github.com>
2025-01-25 20:09:44 +0000

    git push

---
+++ video4/variables.js
@@ -0,0 +1,11 @@
+const accountId = 1234
+let accountEmail = "ayushvik@gmial.com" //{can be sed under scope}
+var accountPassword ="23345353" //global variable
+accountCity ="Bengaluru"
+let accountState
+// accountId = 4321 we cann't change cosnt
+accountEmail ="rageth@gmail.com"
+accountPassword = "46769"
+accountcity = "pune"
+
+console.table([accountId,accountEmail,accountPassword,accountCity,accountState])
---
+++ video5/datatypes.js
@@ -0,0 +1,15 @@
+"use strict";//this indicates you are using new standards of javascript
+//aleart() can be used in browser
+console.log(3+3)
+/* primitive datatypes example:
+number
+bigint
+string =>""
+boolean => true/false
+null=> standlone value typeof null is object
+undefined => typeof undefined is undefined
+symbol => unique
+*/
+/*non-primitive datatypes example:
+object
+*/
