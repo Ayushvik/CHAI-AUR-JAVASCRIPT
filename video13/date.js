c64ae0d0ade64e6641092feb0d346ffcb7e82201
Ayush Vishwakarma <161769397+Ayushvik@users.noreply.github.com>
2025-02-16 12:09:55 +0000

    gitpush

---
+++ video13/date.js
@@ -0,0 +1,7 @@
+let myDate = new Date()
+console.log(myDate)
+console.log(myDate.toString()) // universal time+date
+console.log(myDate.toISOString()) 
+console.log(myDate.toJSON())
+console.log(myDate.toLocaleTimeString()) // tells us time
+console.log(myDate.toDateString()) // sun feb 16 2025
